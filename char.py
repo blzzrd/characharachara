7274d32f1502623ca90e5e0390db044bfc8e8a05
AJC <alexcast001@gmail.com>
2018-12-10 14:12:45 -0800

    Added List Support to store all combinations into a list. Fixed Override bug.

--- char.py
+++ char.py
@@ -1,6 +1,6 @@
 # char.py
 # go through an amount of times and create a list of possible character combinations
-# featuring a-z, 0-9, and '_'
+# featuring a-z, 0-9, and '_'. 
 
 
 # A few constants of our parameters.
@@ -9,6 +9,7 @@ RANGE = 123 # Don't want to go over 123 ASCII
 AFTERNUM = 58 # After the numbers on ASCII
 UNDERSCORE = 95 # Underscore in ASCII
 LETTERSBASE = 97 # Where the letters start in ASCII)
+allcombos = [] # List that holds all the options
 
 # Wrapper function that will input basic information.
 def chara(count):
@@ -36,24 +37,42 @@ def chara_R(string, count, stage):
             if c == (UNDERSCORE + 1):
                 c = LETTERSBASE
 
-            print(string + chr(c))
+
+            allcombos.append(string + chr(c))
+            # print(string + chr(c))
             total += 1
             c += 1
 
     return total
 
 
+# getDigits will get the amount of characters there will be.
+def getDigits(override = False):
+    print("X = 1 | XX = 2 | XXX = 3 ... So on...")
+    digits = input("How many characters should this be out of? ")
+
+    if not override:
+        if digits > 10:
+            print("Too many computations. Toggle Override.")
+            exit()
+    
+    return digits
 
 
-n = 4
+
+
+# Where the work begins
+
+n = getDigits()  # Use getDigits(true) for longer combinations.
 combos = chara(n)
+
 # A-Z + 0-9 + '_' potential combinations to the n'th.
 potential = (26 + 10 + 1) ** n
 
-
 print("Found {} combinations out of the possible {} combinations.".format(combos, potential))
 
-
+for items in allcombos:
+    print(items)
 
 
 # THE HARD CODED METHOD.
@@ -84,5 +103,3 @@ def two():
 total = two()
 print("total outputs: " + str(total))
 '''
-
-
